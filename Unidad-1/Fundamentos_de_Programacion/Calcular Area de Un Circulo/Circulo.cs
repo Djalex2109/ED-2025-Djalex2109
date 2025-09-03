aa1cd423ec3f54be77924c8a9c333f96ce0e06d8
Djalex2109 <alexdm2109@gmail.com>
2025-09-03 10:37:40 -0600

    Add files via upload

---
+++ Unidad-1/Fundamentos_de_Programacion/Calcular Area de Un Circulo/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_Area_de_Un_Circulo
+{
+    internal class Circulo
+    {
+        public double _dblRadio;
+
+        public double Radio
+        {
+            get { return _dblRadio; }
+            set { _dblRadio = value; }
+        }
+        public double Area()
+        {
+            return Math.PI * Math.Pow(_dblRadio, 2);
+        }
+    }
+}
---
+++ Unidad-1/Fundamentos_de_Programacion/Calcular Area de Un Circulo/Program.cs
@@ -0,0 +1,16 @@
+﻿using Calcular_Area_de_Un_Circulo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+
+        Circulo miCirculo = new Circulo();
+       
+        Console.WriteLine("Ingrese el valor de radio:");
+        miCirculo.Radio = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("El area del circulo es: " + miCirculo.Area());
+    }
+}
