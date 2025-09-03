1bcf938e6e304920eb5f079004e49029233e8919
Djalex2109 <alexdm2109@gmail.com>
2025-09-03 10:38:47 -0600

    Add files via upload

---
+++ Unidad-1/Fundamentos_de_Programacion/Grados Celsius a Fahrenheit/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grados_Celsius_a_Fahrenheit;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+
+        Temperatura miTemperatura = new Temperatura();
+        
+        Console.WriteLine("Ingrese la temperatura en grados Celsius:");
+        miTemperatura.Celsius = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("La temperatura en grados Fahrenheit es: " + miTemperatura.ConvertirAFahrenheit()+ "°");
+    }
+}
---
+++ Unidad-1/Fundamentos_de_Programacion/Grados Celsius a Fahrenheit/Temperatura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grados_Celsius_a_Fahrenheit
+{
+    internal class Temperatura
+    {
+        private double _dblCelsius;
+
+        public double Celsius
+        {
+            get { return _dblCelsius; }
+            set { _dblCelsius = value; }
+        }
+        public double ConvertirAFahrenheit()
+        {
+            return (_dblCelsius * 9 / 5) + 32;
+        }
+    }
+}
