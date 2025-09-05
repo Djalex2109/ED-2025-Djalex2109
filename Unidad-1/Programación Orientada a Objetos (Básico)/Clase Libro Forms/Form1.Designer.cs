142f9cb3b6ae2c808a1854aa383e533073c49014
Djalex2109 <alexdm2109@gmail.com>
2025-09-04 20:50:22 -0600

    Add files via upload

---
+++ Unidad-1/Programación Orientada a Objetos (Básico)/Clase Libro Forms/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Clase_Libro_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrearLibro = new System.Windows.Forms.Button();
+            this.btnInformacion = new System.Windows.Forms.Button();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCrearLibro
+            // 
+            this.btnCrearLibro.Location = new System.Drawing.Point(12, 197);
+            this.btnCrearLibro.Name = "btnCrearLibro";
+            this.btnCrearLibro.Size = new System.Drawing.Size(114, 59);
+            this.btnCrearLibro.TabIndex = 0;
+            this.btnCrearLibro.Text = "Crear Libro";
+            this.btnCrearLibro.UseVisualStyleBackColor = true;
+            this.btnCrearLibro.Click += new System.EventHandler(this.btnCrearLibro_Click);
+            // 
+            // btnInformacion
+            // 
+            this.btnInformacion.Location = new System.Drawing.Point(132, 197);
+            this.btnInformacion.Name = "btnInformacion";
+            this.btnInformacion.Size = new System.Drawing.Size(114, 59);
+            this.btnInformacion.TabIndex = 1;
+            this.btnInformacion.Text = "Mostrar Informacion";
+            this.btnInformacion.UseVisualStyleBackColor = true;
+            this.btnInformacion.Click += new System.EventHandler(this.btnInformacion_Click);
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(24, 55);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(132, 22);
+            this.txtTitulo.TabIndex = 2;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(70, 27);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(40, 16);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Titulo";
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(24, 115);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(132, 22);
+            this.txtAutor.TabIndex = 4;
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.Location = new System.Drawing.Point(70, 96);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Size = new System.Drawing.Size(38, 16);
+            this.lblAutor.TabIndex = 5;
+            this.lblAutor.Text = "Autor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 268);
+            this.Controls.Add(this.lblAutor);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.btnInformacion);
+            this.Controls.Add(this.btnCrearLibro);
+            this.Name = "Form1";
+            this.Text = "Libro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrearLibro;
+        private System.Windows.Forms.Button btnInformacion;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.Label lblAutor;
+    }
+}
+
---
+++ Unidad-1/Programación Orientada a Objetos (Básico)/Clase Libro Forms/Libro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_Libro_Forms
+{
+    internal class Libro
+    {
+        private string _strTitulo;
+        private string _strAutor;
+
+        public string Titulo
+        {
+            get { return _strTitulo; }
+            set { _strTitulo = value; }
+        }
+        public string Autor
+        {
+            get { return _strAutor; }
+            set { _strAutor = value; }
+        }
+        public Libro(string strTitulo, string strAutor)
+        {
+            _strTitulo = strTitulo;
+            _strAutor = strAutor;
+        }
+        public override string ToString()
+        {
+            return ("Titulo del libro: " + Titulo + "\nNombre del autor: " + Autor);
+        }
+        ~Libro()
+        {            
+            MessageBox.Show($"El libro '{Titulo}' de {Autor} se ha destruido",
+                "Destructor", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
