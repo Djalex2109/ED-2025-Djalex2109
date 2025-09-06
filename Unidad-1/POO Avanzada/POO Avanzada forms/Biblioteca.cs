291e147793f42488cd2af74011d5a2fef3b35ea8
Djalex2109 <alexdm2109@gmail.com>
2025-09-05 20:15:52 -0600

    Add files via upload

---
+++ Unidad-1/POO Avanzada/POO Avanzada forms/Biblioteca.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Avanzada_forms
+{
+    internal class Biblioteca
+    {
+        private DateTime _dtmFechaPrestamo;
+        private DateTime _dtmFechaDevolucion;
+
+        public DateTime FechaPrestamo
+        {
+            get { return _dtmFechaPrestamo; }
+            set { _dtmFechaPrestamo = value; }
+        }
+        public DateTime FechaDevolucion
+        {
+            get { return _dtmFechaDevolucion; }
+            set { _dtmFechaDevolucion = value; }
+        }
+
+        public void Prestar(Libro strlibro, Usuario strusuario)
+        {
+            FechaPrestamo = DateTime.Now;
+            FechaDevolucion = FechaPrestamo.AddDays(7); 
+            Console.WriteLine($"{strusuario.Nombre} ha prestado '{strlibro.Titulo}'");
+            Console.WriteLine($"Fecha de devolución: {FechaDevolucion.ToShortDateString()}");
+        }
+        public void Devolver(Libro strlibro, Usuario strusuario)
+        {
+            FechaPrestamo = DateTime.MinValue;
+            FechaDevolucion = DateTime.MinValue;
+            Console.WriteLine($"{strusuario.Nombre} ha devuelto '{strlibro.Titulo}'");
+        }
+    }
+}
---
+++ Unidad-1/POO Avanzada/POO Avanzada forms/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace POO_Avanzada_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxUsuario = new System.Windows.Forms.GroupBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.groupBoxLibro = new System.Windows.Forms.GroupBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.lblISBN = new System.Windows.Forms.Label();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.dtpFechaDeDevolucion = new System.Windows.Forms.DateTimePicker();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnPrestar = new System.Windows.Forms.Button();
+            this.btnDevolver = new System.Windows.Forms.Button();
+            this.groupBoxUsuario.SuspendLayout();
+            this.groupBoxLibro.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxUsuario
+            // 
+            this.groupBoxUsuario.Controls.Add(this.lblId);
+            this.groupBoxUsuario.Controls.Add(this.lblNombre);
+            this.groupBoxUsuario.Controls.Add(this.txtId);
+            this.groupBoxUsuario.Controls.Add(this.txtNombre);
+            this.groupBoxUsuario.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxUsuario.Name = "groupBoxUsuario";
+            this.groupBoxUsuario.Size = new System.Drawing.Size(268, 100);
+            this.groupBoxUsuario.TabIndex = 0;
+            this.groupBoxUsuario.TabStop = false;
+            this.groupBoxUsuario.Text = "Usuario";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(29, 81);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(18, 16);
+            this.lblId.TabIndex = 3;
+            this.lblId.Text = "Id";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(6, 44);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(106, 78);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(136, 22);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(106, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(136, 22);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // groupBoxLibro
+            // 
+            this.groupBoxLibro.Controls.Add(this.txtISBN);
+            this.groupBoxLibro.Controls.Add(this.lblISBN);
+            this.groupBoxLibro.Controls.Add(this.lblAutor);
+            this.groupBoxLibro.Controls.Add(this.lblTitulo);
+            this.groupBoxLibro.Controls.Add(this.txtAutor);
+            this.groupBoxLibro.Controls.Add(this.txtTitulo);
+            this.groupBoxLibro.Location = new System.Drawing.Point(301, 12);
+            this.groupBoxLibro.Name = "groupBoxLibro";
+            this.groupBoxLibro.Size = new System.Drawing.Size(268, 145);
+            this.groupBoxLibro.TabIndex = 1;
+            this.groupBoxLibro.TabStop = false;
+            this.groupBoxLibro.Text = "Libro";
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(106, 117);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(136, 22);
+            this.txtISBN.TabIndex = 5;
+            // 
+            // lblISBN
+            // 
+            this.lblISBN.AutoSize = true;
+            this.lblISBN.Location = new System.Drawing.Point(8, 120);
+            this.lblISBN.Name = "lblISBN";
+            this.lblISBN.Size = new System.Drawing.Size(38, 16);
+            this.lblISBN.TabIndex = 4;
+            this.lblISBN.Text = "ISBN";
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.Location = new System.Drawing.Point(8, 81);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Size = new System.Drawing.Size(38, 16);
+            this.lblAutor.TabIndex = 3;
+            this.lblAutor.Text = "Autor";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(6, 44);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(40, 16);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Titulo";
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(106, 78);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(136, 22);
+            this.txtAutor.TabIndex = 1;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(106, 41);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(136, 22);
+            this.txtTitulo.TabIndex = 0;
+            // 
+            // dtpFechaDeDevolucion
+            // 
+            this.dtpFechaDeDevolucion.Location = new System.Drawing.Point(590, 12);
+            this.dtpFechaDeDevolucion.Name = "dtpFechaDeDevolucion";
+            this.dtpFechaDeDevolucion.Size = new System.Drawing.Size(259, 22);
+            this.dtpFechaDeDevolucion.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(59, 226);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(115, 54);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnPrestar
+            // 
+            this.btnPrestar.Location = new System.Drawing.Point(193, 226);
+            this.btnPrestar.Name = "btnPrestar";
+            this.btnPrestar.Size = new System.Drawing.Size(115, 54);
+            this.btnPrestar.TabIndex = 4;
+            this.btnPrestar.Text = "Prestar";
+            this.btnPrestar.UseVisualStyleBackColor = true;
+            this.btnPrestar.Click += new System.EventHandler(this.btnPrestar_Click);
+            // 
+            // btnDevolver
+            // 
+            this.btnDevolver.Location = new System.Drawing.Point(339, 226);
+            this.btnDevolver.Name = "btnDevolver";
+            this.btnDevolver.Size = new System.Drawing.Size(115, 54);
+            this.btnDevolver.TabIndex = 5;
+            this.btnDevolver.Text = "Devolver";
+            this.btnDevolver.UseVisualStyleBackColor = true;
+            this.btnDevolver.Click += new System.EventHandler(this.btnDevolver_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 278);
+            this.Controls.Add(this.btnDevolver);
+            this.Controls.Add(this.btnPrestar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dtpFechaDeDevolucion);
+            this.Controls.Add(this.groupBoxLibro);
+            this.Controls.Add(this.groupBoxUsuario);
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.Name = "Form1";
+            this.Text = "Biblioteca";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxUsuario.ResumeLayout(false);
+            this.groupBoxUsuario.PerformLayout();
+            this.groupBoxLibro.ResumeLayout(false);
+            this.groupBoxLibro.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxUsuario;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.GroupBox groupBoxLibro;
+        private System.Windows.Forms.Label lblAutor;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.DateTimePicker dtpFechaDeDevolucion;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnPrestar;
+        private System.Windows.Forms.Button btnDevolver;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Label lblISBN;
+    }
+}
+
