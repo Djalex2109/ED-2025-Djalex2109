546ce46a4d3f594e8e49d02c4b32078ef0397ce7
Djalex2109 <alexdm2109@gmail.com>
2025-09-08 07:05:24 -0600

    Add files via upload

---
+++ Unidad-1/ejercicio de diagnóstico/Ejercicio En Clase 8-sep.2025 Ed/Cuadrado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_En_Clase_8_sep._2025_Ed
+{
+    internal class Cuadrado : Rectangulo
+    {
+        public Cuadrado(double lado) : base(lado, lado)
+        {
+            lado = Ancho;
+        }
+        public Cuadrado(double ancho, double alto) : base(ancho, alto)
+        {
+           
+        }
+
+    }
+}
---
+++ Unidad-1/ejercicio de diagnóstico/Ejercicio En Clase 8-sep.2025 Ed/Rectangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_En_Clase_8_sep._2025_Ed
+{
+    internal class Rectangulo
+    {
+        private double _dblAncho;
+        private double _dblAlto;
+
+        public double Ancho
+        {
+            get { return _dblAncho; }
+            set { _dblAncho = value; }
+        }
+        public double Alto
+        {
+            get { return _dblAlto; }
+            set { _dblAlto = value; }
+        }
+
+        public Rectangulo(double dblAncho, double dblAlto)
+        {
+            _dblAncho = dblAncho;
+            _dblAlto = dblAlto;
+        }
+
+        public double MostrarArea()
+        {
+            return _dblAncho * _dblAlto;
+        }
+        public double MostrarPerimetro()
+        {
+            return 2 * (_dblAncho + _dblAlto);
+        }
+        public string MostrarDimensiones()
+        {
+            return $"Ancho: {_dblAncho}, Alto: {_dblAlto}";
+        }
+    }
+}
